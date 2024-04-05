eeedf0cdec7b14a287ee6400dcd993357dee8cae
Allan Borges Marques - Developer | DBA | Help Desk <90579145+Allanads@users.noreply.github.com>
2024-04-05 01:59:18 -0300

    Refactor: Incluido o frontend na tela home e login.

---
+++ login.php
@@ -0,0 +1,131 @@
+<?php
+
+include('conexao.php');
+
+?>
+<!DOCTYPE HTML>
+<html lang="pt">
+
+<head>
+	<title>WebSite SEGURADORA ATK</title>
+	<meta charset="utf-8">
+	<meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body class="subpage">
+
+	<!-- Header -->
+
+	<header id="header" class="alt">
+		<div class="logo"><a href="index.html">WebSite <span>by ATK Company</span></a></div>
+		<a href="#menu"><span>Menu</span></a>
+	</header>
+	<!-- Nav -->
+	<nav id="menu">
+		<ul class="links">
+			<li><a href="index.html">Home</a></li>
+		</ul>
+	</nav>
+	<!-- Content -->
+	<!--
+            Note: To show a background image, set the "data-bg" attribute below
+            to the full filename of your image. This is used in each section to set
+            the background image.
+        -->
+	<section id="post" class="wrapper bg-img" data-bg="banner2.jpg">
+		<div class="inner">
+			<article class="box">
+				<header>
+					<center>
+						<h2>FAÇA O SEU LOGIN</h2>
+
+						<br>
+
+						<?php
+						if (isset($_POST['email']) || isset($_POST['senha'])) {
+
+							if (strlen($_POST['email']) == 0) {
+								echo "Preencha seu e-mail";
+							} else if (strlen($_POST['senha']) == 0) {
+								echo "Preencha sua senha";
+							} else {
+
+								$email = $conn->real_escape_string($_POST['email']);
+								$senha = $conn->real_escape_string($_POST['senha']);
+
+								$sql_code = "SELECT * FROM e0_usuario WHERE email = '$email' AND senha = '$senha'";
+								$sql_query = $conn->query($sql_code) or die("Falha na execução do código SQL: " . $conn->error);
+								$quantidade = $sql_query->num_rows;
+
+								if ($quantidade == 1) {
+
+									$e0_usuario = $sql_query->fetch_assoc();
+
+									if (!isset($_SESSION)) {
+										session_start();
+									}
+
+									$_SESSION['user'] = $e0_usuario['id'];
+									$_SESSION['nome'] = $e0_usuario['nome'];
+
+									header("Location: home.php");
+								} else {
+									echo "<span style='color: red;'>Falha ao Logar! E-mail ou senha incorretos</span>";
+								}
+							}
+						}
+
+						?>
+
+						<form action="" method="POST">
+							<p>
+								<label>E-mail</label>
+								<input type="text" name="email">
+							</p>
+							<p>
+								<label>Senha</label>
+								<input type="password" name="senha">
+							</p>
+							<p>
+								<style>
+									.botao-verde {
+										background-color: #03ad61;
+									}
+								</style>
+
+								<a href="home.php"><button class="botao-verde">ENTRAR</button></a>
+								<br>
+
+							</form>
+							</p>
+						</form>
+
+				</header>
+				<center>
+				<style>
+					.botao-azul-claro {
+						background-color: #03619e;
+					}
+				</style>
+				<form action="usuario/usuario.html">
+					<button class="botao-azul-claro">INCLUIR USUÁRIO OU ALTERAR DADOS</button>
+				</form>
+				</center>
+			</article>
+		</div>
+	</section>
+	<div class="copyright">
+		WebSite <a>By Company ATK</a>
+	</div>
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/jquery.scrollex.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+</body>
+
+</html>
--- protect.php
+++ protect.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)) {
 }
 
 if(!isset($_SESSION['id'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
+    die("Você não pode acessar esta página porque não está logado.<p><a href=\"login.php\">Entrar</a></p>");
 }
 
 
