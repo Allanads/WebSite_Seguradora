bb4f87a8d8ae4df3cc05e39878059785a164b84d
Allan Borges Marques - Developer | DBA | Help Desk <90579145+Allanads@users.noreply.github.com>
2024-04-27 01:49:10 -0300

    Refactor: Alterando o background

--- automovel/vei_pro.php
+++ automovel/vei_pro.php
@@ -1,24 +1,27 @@
-
 <?php
 session_start();
 include_once("../conexao.php");
 
-$cod = filter_input(INPUT_POST, 'cod', FILTER_SANITIZE_NUMBER_INT);
+
 $placa = filter_input(INPUT_POST, 'placa', FILTER_SANITIZE_STRING);
 $renavan = filter_input(INPUT_POST, 'renavan', FILTER_SANITIZE_STRING);
 $fabricante = filter_input(INPUT_POST, 'fabricante', FILTER_SANITIZE_STRING);
 $modelo = filter_input(INPUT_POST, 'modelo', FILTER_SANITIZE_STRING);
 $ano = filter_input(INPUT_POST, 'ano', FILTER_SANITIZE_NUMBER_INT);
 
-
-$result_veiculo = "INSERT INTO e2_veiculos(cod,placa,renavan,fabricante,modelo,ano) VALUES ('$cod', '$placa', '$renavan','$fabricante','$modelo','$ano')";
+$result_veiculo = "INSERT INTO e2_veiculos(placa,renavan,fabricante,modelo,ano) VALUES ('$placa', '$renavan','$fabricante','$modelo','$ano')";
 $resultado_veiculo = mysqli_query($conn, $result_veiculo);
 
+if (!$resultado_veiculo) {
+    // Exibir mensagem de erro do MySQL
+    echo "Erro ao cadastrar veículo: " . mysqli_error($conn);
+    exit;
+}
+
 if($conn->affected_rows == 1){
-	$_SESSION['msg'] = "<h2><p style='color:green;'>Veículo cadastrado com sucesso.</p></h2>";
+    $_SESSION['msg'] = "<h2><p style='color:green;'>Veículo cadastrado com sucesso.</p></h2>";
 }else{
-	$_SESSION['msg'] = "<h2><p style='color:red;'>Veículo não foi cadastrado!!!</p></h2>";
+    $_SESSION['msg'] = "<h2><p style='color:red;'>Veículo não foi cadastrado!!!</p></h2>";
 }
 header("Location: vei_cad.php");
-
-
+?>
--- cliente/cli_pro.php
+++ cliente/cli_pro.php
@@ -1,23 +1,18 @@
-
 <?php
 session_start();
 include_once("../conexao.php");
 
-$cod = filter_input(INPUT_POST, 'cod', FILTER_SANITIZE_NUMBER_INT);
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
 $rg = filter_input(INPUT_POST, 'rg', FILTER_SANITIZE_STRING);
 
-
-
-$result_cliente = "INSERT INTO e1_cliente(cod,nome,cpf,rg,tel) VALUES ('$cod','$nome', '$cpf','$rg','$tel')";
+$result_cliente = "INSERT INTO e1_cliente(nome, cpf, rg) VALUES ('$nome', '$cpf', '$rg')";
 $resultado_cliente = mysqli_query($conn, $result_cliente);
 
-
-if($conn->affected_rows == 1){
-	$_SESSION['msg'] = "<h2><p style='color:green;'>Cliente cadastrado com sucesso!</p></h2>";
+if($resultado_cliente){
+    $_SESSION['msg'] = "<h2><p style='color:green;'>Cliente cadastrado com sucesso!</p></h2>";
 }else{
-	$_SESSION['msg'] = "<h2><p style='color:red;'>Cliente não foi cadastrado!!!</p></h2>";
+    $_SESSION['msg'] = "<h2><p style='color:red;'>Cliente não foi cadastrado!!!</p></h2>";
 }
 header("Location: cli_cad.php");
-
+?>
