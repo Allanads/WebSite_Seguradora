f02e507d5c3849ae7a859bfe3649195842ff784c
Allan Borges Marques - Developer | DBA | Help Desk <90579145+Allanads@users.noreply.github.com>
2024-04-27 19:04:55 -0300

    Refactor: iniciando o frontend e algumas mudanças nos arquivos de ocorrências.

--- ocorrencias/oco_cad.php
+++ ocorrencias/oco_cad.php
@@ -1,88 +1,96 @@
-<?php
-session_start();
-//include_once("../validaCPF.php")
-?>
-<!DOCTYPE html>
-<html>
-<META charset="utf-8">
+<!DOCTYPE HTML>
+<html lang="pt">
+
 <head>
-	<title>SEGURADORA</title>
-	<link rel="stylesheet" href="css/estilos.css">
-	<link href="formata.css" rel="stylesheet">
+    <title>WebSite Seguradora ATK</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    <style>
+        /* Estilo para corrigir a cor do texto selecionado dentro do input */
+        input[type="date"]::-webkit-datetime-edit-text {
+            color: black;
+        }
+        
+        /* Estilo para corrigir o fundo do input */
+        input[type="date"]::-webkit-calendar-picker-indicator {
+            background-color: white;
+        }
+    </style>
 </head>
 
-<center>
-<header class="cabecalho">
-	<h1 class="titulo" align="center">SEGURADORA</h1>
-	<br>
-	<h2 class="titulo" align="center">ACIDENTES NÃO ACONTECEM POR ACASO, MAS POR DESCASO!!!!</h2>
-	<br>
-	<h2 class="titulo" align="center">INCLUSÃO DE OCORRÊNCIAS</h2>
+<body class="is-preload">
 
-</header>
-<hr>
-<br>
+    <!-- Wrapper -->
+    <div id="wrapper">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <!-- Adicionando botões no topo da página -->
+        <div id="top-buttons">
+            <button onclick="window.location.href='../home.html'">Tela Inicial</button>
+            <button onclick="window.location.href='automovel.html'">Voltar</button>
+            <button onclick="window.location.href='../index.html'">Sair</button>
+        </div>
+        <br>
+        <!-- Header -->
+        <header id="header">
+            <div class="logo">
+                <span class="icon"><i class="fas fa-car-side"></i></span>
+            </div>
+            <div class="content">
+                <div class="inner">
+                    <h1>Inclusão das ocorrências</h1>
+                </div>
+            </div>
+            <br>
+        </header>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+        <?php 
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        }
+        ?>
 
-		<?php
-		if(isset($_SESSION['msg'])){
-			echo $_SESSION['msg'];
-			unset($_SESSION['msg']);
-		}
-		?>
+        <div class="form-container-oco">
+            <form method="POST" action="oco_pro.php">
 
-		<div class="form-container">
-			<form method="POST" action="oco_pro.php">
+                <label>Nome do cliente: </label>
+                <input type="text" name="placa" maxlength="8" required placeholder="Digite o seu placa"><br>
 
-				<label>Número da Ocorrência: </label>
-				<input  type="number" required name="nr_ocor" placeholder="Digite o número da ocorrência"><br><br>
-				
-				<label>Data: </label>
-				<input  type="date" name="data" required placeholder="Digite a data da ocorrência"><br><br>
+                <label>Placa do veículo: </label>
+                <input type="text" name="placa" maxlength="8" required placeholder="Digite o seu placa"><br>
 
-				<label>local: </label>
-				<input type="text" name="local" required maxlength="50" placeholder="Digite o local da ocorrencia"><br><br>
+                <label>Data do ocorrido: </label>
+                <input type="date" name="data" required placeholder="Digite a data da ocorrência" value="<?php echo date('Y-m-d'); ?>"><br><br>
 
-				<label>descr: </label>
-				<input type="text" name="descr" required maxlength="100" placeholder="Digite a descrição da ocorrencia"><br><br>	
+                <label>local do ocorrido: </label>
+                <input type="text" name="local" required maxlength="50" placeholder="Digite o local da ocorrencia"><br>
 
-				<input type="submit" value="Cadastrar">
-			</form>
-		</div>
- <br>
+                <label>Descrição do ocorrido: </label>
+                <input type="text" name="descr" required maxlength="100" placeholder="Digite a descrição da ocorrencia"><br>
+                <center>
+                    <input type="submit" value="INCLUIR">
+            </form>
+        </div>
 
-<br><br><hr>
-	 
-<a href="ocorrencias.html"> <img src="../img/retornar.png" width="30" height="30">  </a>
+        <!-- Footer -->
+        <footer id="footer">
+            <p class="copyright"> WebSite By Company ATK</p>
+        </footer>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+    </div>
 
-<footer>
-<p style='color:#808080'>&copy; Copyright  <script>var year=new Date();document.writeln(+year.getUTCFullYear());</script></p>
-</footer>
+    <!-- BG -->
+    <div id="bg"></div>
 
-</center>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
 
 </body>
-</html>
 
+</html>
